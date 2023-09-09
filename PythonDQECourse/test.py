7196c0f06ed65ce07d6d431d95d0ad2d021e6a10
Ismail Sajjad <IsmaiSajjad@gmail.com>
2023-09-09 15:03:35 +0200

    testing file

--- PythonDQECourse/test.py
+++ PythonDQECourse/test.py
@@ -1,3 +1,5 @@
+# #this file is created for testing purpose
+
 dict1 = {'a': 5, 'b': 3, 'c': 7}
 dict2 = {'b': 8, 'c': 4, 'd': 6}
 
@@ -20,3 +22,4 @@ for key, value in dict2.items():
 
 # Print the merged dictionary
 print(merged_dict)
+
