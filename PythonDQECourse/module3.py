da65aeea28221d8fa529301a837ec02dfd07e0ed
Ismail Sajjad <IsmaiSajjad@gmail.com>
2023-09-13 08:57:22 +0200

    module3

--- PythonDQECourse/module3.py
+++ PythonDQECourse/module3.py
@@ -26,5 +26,28 @@ print(bb.isalnum())         #it checks if string is integer
 print(bb.isalpha())         #it checks if string is only alphabet
 
 # #Modify Method replace(), split(), splitlines(), join(), format()
+# cc = "I am in poland com"
+# cc.replace(' ', '/n')           #strings cannot be mutated or changed. We can assign strings to variables, and reassign new strings to the same variable, but individual characters within a string cannot be reassigned.
+# print(cc)
+# dd = cc.replace(' ', '\n')
+# print(dd)
+# ee = cc.replace('i', '**')  #replacing with symbol  answer: I am **n poland com
+# print(ee)
+# #split()
+# ff = "i am in poland com"
+# print(ff.split('i')) #it will replace i and split the string answer: ['', ' am ', 'n poland com']
+# print(ff.split())   #it will split the string where space is mention  answer: ['i', 'am', 'in', 'poland', 'com']
+# print(ff.split('i')[2]) #it means start from index 2 and replace i from that index and print rest of values result: n poland com
+
+# #splitlines
+ee = "i am in \npoland\n com"
+print(ee.splitlines())    #results ['i am in ', 'poland', ' com']
+
+# #join join different symbol in one string
+gg = ['a','b','c']
+gf = ''.join(gg)
+print(gf)   #abc
+import os
+print(os.path.join("C:","User","IsmailSajjad"))
 
 
